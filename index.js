df99a4e65f7b7a8986522edf8e9aa2d205b0549e
b tsagueu <brandeltsagueu@gmail.com>
2021-07-14 09:22:34 +0200

    begin to edit my first class: Provider

--- index.js
+++ index.js
@@ -31,6 +31,75 @@
   document.querySelector('.standort').append( cityName , menu_weather_logo_div );
 
 
+  const getinput = document.createElement('div');
+  getinput.id = 'menu-getinput';
+
+  const input = document.createElement('input');
+  input.id = 'input-storage';  
+  input.required = true;
+  input.placeholder = 'Beliebten Standort ändern';
+
+  const submit = document.createElement('input');
+  submit.type = "button";
+  submit.value = "Ok";
+  submit.id = 'btn-storage';
+
+  getinput.append(input, submit);
+  menu_header.append(getinput); 
+  document.querySelector('#menu-getinput').style.display = 'none';
+
+document.body.style.fontFamily = 'tahoma';
+
+// class WeatherApp 
+// {
+//     constructor() {
+
+//     }
+
+// }
+
+      
+let p = document.createElement('p');
+    const p_content = document.createTextNode("Der Stadtname ist falsch");
+    p.style.width = '100vw';
+    p.style.height = '20px';
+    p.style.color = 'white';
+    p.style.backgroundColor = 'red';
+    p.style.fontFamily = 'tahoma';
+    p.style.fontSize = '1rem';
+    p.style.position = 'fixed';
+    p.style.bottom = '0';
+    p.style.textAlign = 'center';
+    p.style.display = 'block';
+    p.appendChild(p_content);
+    document.body.appendChild(p);
+//3) ************** PROVIDER **************
+function Provider( apiKey )
+{
+    this.apiKey = apiKey;
+}
+Provider.prototype.fetchWeatherByCityName = function( cityName ) {
+
+    let fetchedCurrentData = {};
+    fetch("http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&lang=de&appid="+ this.apiKey)
+    .then( (response) => {
+        console.log(response)
+        if ( response.ok ) 
+            return response.json();
+    },
+    (reason) => console.log(new Error('fetch failed : ', reason)))
+    .then( (data) => {
+        fetchedCurrentData = data;
+        new WorkData().displayCurrentWeather(data);
+        this.fetchWeatherByGeolocation(data.coord.lat, data.coord.lon); 
+        document.body.removeChild(p);
+        return fetchedCurrentData;
+    })
+    .catch((reason) => { 
+        console.log(reason); 
+    });   
+
+}
 
 
 
